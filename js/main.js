f585b5a5bb5bda849890642fc1b88d269d288caf
Yacith-D <martinezyacith@gmail.com>
2024-05-16 22:21:36 -0600

    creando funcion para el juego

---
+++ js/main.js
@@ -0,0 +1,96 @@
+let cuadro1 = document.getElementById("cuadrito1")
+let cuadro2 = document.getElementById("cuadrito2")
+let cuadro3 = document.getElementById("cuadrito3")
+let cuadro4 = document.getElementById("cuadrito4")
+let cuadro5 = document.getElementById("cuadrito5")
+let cuadro6 = document.getElementById("cuadrito6")
+let cuadro7 = document.getElementById("cuadrito7")
+let cuadro8 = document.getElementById("cuadrito8")
+let cuadro9 = document.getElementById("cuadrito9")
+
+let clave1 = true
+let tt = true
+
+function funcion1() {
+    if (clave1 === true) {
+        if (tt === true) {
+            cuadro1.innerHTML = 'X'
+            tt = false;
+        } else {
+            cuadro1.innerHTML = 'O'
+            tt = true;
+        }
+        clave1 = false;
+    } else {
+        console.log(':(');
+    }
+    }
+
+
+
+    let clave2 = true
+
+
+function funcion2() {
+    if (clave2 === true) {
+        if (tt === true) {
+            cuadro2.innerHTML = 'X'
+            tt = false;
+        } else {
+            cuadro2.innerHTML = 'O'
+            tt = true;
+        }
+        clave2 = false;
+    } else {
+        console.log(':(');
+    }
+    }
+
+
+
+
+
+    let clave3 = true
+
+
+function funcion3() {
+    if (clave3 === true) {
+        if (tt === true) {
+            cuadro3.innerHTML = 'X'
+            tt = false;
+        } else {
+            cuadro3.innerHTML = 'O'
+            tt = true;
+        }
+        clave3 = false;
+    } else {
+        console.log(':(');
+    }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
